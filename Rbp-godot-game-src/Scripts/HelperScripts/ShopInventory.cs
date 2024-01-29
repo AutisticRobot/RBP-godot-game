a86e3db0e5dcd1374481f7e425cc12a82f475304
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-01-28 22:14:28 -0800

    worked on shop menu

--- Rbp-godot-game-src/Scripts/HelperScripts/ShopInventory.cs
+++ Rbp-godot-game-src/Scripts/HelperScripts/ShopInventory.cs
@@ -15,6 +15,26 @@ public partial class ShopInventory : Node
 	public int Spices;
 	[Export]
 	public int Jewlery;
+	[Export]
+	public int FoodSell;
+	[Export]
+	public int RumSell;
+	[Export]
+	public int LinensSell;
+	[Export]
+	public int SpicesSell;
+	[Export]
+	public int JewlerySell;
+	[Export]
+	public int FoodBuy;
+	[Export]
+	public int RumBuy;
+	[Export]
+	public int LinensBuy;
+	[Export]
+	public int SpicesBuy;
+	[Export]
+	public int JewleryBuy;
 
 
 }
--- Rbp-godot-game-src/Scripts/ObjectScripts/ShopMenu.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/ShopMenu.cs
@@ -3,7 +3,10 @@ using System;
 
 public partial class ShopMenu : Control
 {
+	[Export]
 	public ShopInventory shop;
+	[Export]
+	public inventory player;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
