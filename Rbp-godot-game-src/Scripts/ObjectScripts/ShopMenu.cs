3bf2b1e8cd95edfb3965d5b540cc59579d529f54
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-02-02 21:50:25 -0800

    finished first version of the shop menu

--- Rbp-godot-game-src/Scripts/ObjectScripts/ShopMenu.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/ShopMenu.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 using System.Linq;
 
@@ -10,6 +11,15 @@ public partial class ShopMenu : Control
 	public inventory player;
 	[Export]
 	public int buyMulti;
+	public Godot.Collections.Dictionary<string, int> indexList = new()
+    {
+		{"Food", 0},
+		{"Jewlery", 1},
+		{"Linens", 2},
+		{"Money", 3},
+		{"Rum", 4},
+		{"Spices", 5},
+	};
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -22,18 +32,92 @@ public partial class ShopMenu : Control
 	{
 	}
 
-	public void exchange(bool isBuy, string type)
+	public void Exchange(bool isBuy, string type)
 	{
+		int buyerWallet;//buyer money
+		int buyer;//buyer stock
+		int seller;//seller stock
+		int sellerWallet;//seller stock
+		int price;//price
 
+		if(isBuy)
+		{
+			buyerWallet = player.inv["Money"];
+			sellerWallet = shop.inv["Money"];
+			buyer = player.inv[type];
+			seller = shop.inv[type];
+			price = shop.buy[type];
+		}else{
+			buyer = shop.inv[type];
+			seller = player.inv[type];
+			buyerWallet = shop.inv["Money"];
+			sellerWallet = player.inv["Money"];
+			price = shop.sell[type];
+		}
+
+		GD.Print("before:");
+		GD.Print(buyer);
+		GD.Print(buyerWallet);
+		GD.Print(seller);
+		GD.Print(sellerWallet);
+		GD.Print(price);
+		GD.Print("*-----*");
+
+		if(buyerWallet >= price * buyMulti && seller >= buyMulti)
+		{
+			buyerWallet -= price * buyMulti;
+			sellerWallet += price * buyMulti;
+			buyer += buyMulti;
+			seller -= buyMulti;
+		}else{
+			int curMulti;
+			int canAfford = buyerWallet / price;
+
+			GD.Print(buyerWallet);
+			GD.Print(price);
+			GD.Print(canAfford);
+
+			if(canAfford > seller)
+			{
+				curMulti = seller;
+			}else{
+				curMulti = canAfford;
+			}
+
+			buyerWallet -= price * curMulti;
+			sellerWallet += price * curMulti;
+			buyer += curMulti;
+			seller -= curMulti;
+		}
+
+
+		if(isBuy)
+		{
+			player.inv[type] = buyer;
+			player.inv["Money"] = buyerWallet;
+			shop.inv["Money"] = sellerWallet;
+			shop.inv[type] = seller;
+		}else{
+			player.inv[type] = seller;
+			player.inv["Money"] = sellerWallet;
+			shop.inv["Money"] = buyerWallet;
+			shop.inv[type] = buyer;
+		}
 	}
 
-	public int getShopInv(string type, int valState)
+	public int GetShopInv(string type, int valState)
 	{
-		return 0;
-	}
-	public int getPlayerInv(string type)
+        return valState switch
+        {
+            0 => shop.inv[type],
+            1 => shop.buy[type],
+            2 => shop.sell[type],
+            _ => 0,
+        };
+    }
+    public int GetPlayerInv(string type)
 	{
-		return 0;
+		return player.inv[type];
 	}
 
 }
--- Rbp-godot-game-src/Scripts/ObjectScripts/shopListing.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/shopListing.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class shopListing : Sprite2D
 {
@@ -7,19 +8,49 @@ public partial class shopListing : Sprite2D
 	public ShopMenu shopMan;
 	[Export]
 	public string type;
+	[Export]
+	public Label playerStock;
+	[Export]
+	public Label buyPrice;
+	[Export]
+	public Label sellPrice;
+	[Export]
+	public Label shopStock;
+	[Export]
+	public bool onlyShow;
 
 
 	public override void _Ready()
 	{
 		shopMan = GetParent<ShopMenu>();
+
+	}
+
+	
+	public override void _Process(double delta)
+	{
+		Update();
+	}
+
+	public void Update()
+	{
+		playerStock.Text = shopMan.GetPlayerInv(type).ToString();
+		shopStock.Text = shopMan.GetShopInv(type, 0).ToString();
+		if(!onlyShow)
+		{
+			buyPrice.Text = shopMan.GetShopInv(type, 1).ToString();
+			sellPrice.Text = shopMan.GetShopInv(type, 2).ToString();
+		}
 	}
 
 	public void Buy()
 	{
-		shopMan.exchange(true, type);
+		GD.Print("Buy");
+		shopMan.Exchange(true, type);
 	}
 	public void Sell()
 	{
-		shopMan.exchange(false, type);
+		GD.Print("Sell");
+		shopMan.Exchange(false, type);
 	}
 }
