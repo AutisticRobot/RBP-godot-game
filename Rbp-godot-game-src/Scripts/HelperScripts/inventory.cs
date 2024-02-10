f57218bef27be7d4b349f8ed62a54a93a18506ac
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-02-09 20:59:32 -0800

    added get/set functions to inventory indexer

--- Rbp-godot-game-src/Scripts/HelperScripts/inventory.cs
+++ Rbp-godot-game-src/Scripts/HelperScripts/inventory.cs
@@ -19,11 +19,48 @@ public partial class inventory : Resource
 	{
 		get
 		{
-			return 0;
+			return i switch
+			{
+				0 => Money,
+				1 =>Food,
+				2 =>Rum,
+				3 =>Linens,
+				4 =>Spices,
+				5 =>Jewlery,
+				_ => 0
+			};
 		}
 		set
 		{
-			
+			switch(i)
+			{
+                case 0:
+					Money = value;
+				break;
+
+                case 1:
+					Food = value;
+				break;
+
+                case 2:
+					Rum = value;
+				break;
+
+                case 3:
+					Linens = value;
+				break;
+
+                case 4:
+					Spices = value;
+				break;
+
+                case 5:
+					Jewlery = value;
+				break;
+				
+				default:
+				break;
+			}
 		}
 	}
 }
--- Rbp-godot-game-src/Scripts/ObjectScripts/basicHud.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/basicHud.cs
@@ -15,13 +15,13 @@ public partial class basicHud : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var player = ship.inv.inv;
-		setLabelCont(0, "Gold", player["Money"]);
-		setLabelCont(1, "Food", player["Food"]);
-		setLabelCont(2, "Rum", player["Rum"]);
-		setLabelCont(3, "Linens", player["Linens"]);
-		setLabelCont(4, "Spices", player["Spices"]);
-		setLabelCont(5, "Jewlery", player["Jewlery"]);
+		var player = ship.inv;
+		setLabelCont(0, "Gold", player.Money);
+		setLabelCont(1, "Food", player.Food);
+		setLabelCont(2, "Rum", player.Rum);
+		setLabelCont(3, "Linens", player.Linens);
+		setLabelCont(4, "Spices", player.Spices);
+		setLabelCont(5, "Jewlery", player.Jewlery);
 	}
 
 	private void setLabelCont(int index, string res, int cnt)
