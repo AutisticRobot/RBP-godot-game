69e162bea72beea309b9f1a41f82900c82064968
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-02-13 21:44:39 -0800

    worked on save file system

--- Rbp-godot-game-src/Scripts/SaveSystem/SceneSave.cs
+++ Rbp-godot-game-src/Scripts/SaveSystem/SceneSave.cs
@@ -5,13 +5,32 @@ using System;
 public partial class SceneSave : Node
 {
 	[Export] public string SaveFolder;
-	// Called when the node enters the scene tree for the first time.
+	[Export] public string SaveFile;
+
+
+	public FileAccess File;
+	public Global global;
+
+
+
 	public override void _Ready()
+	{
+		global = GetNode<Global>("/root/Global");
+		//TODO learn the ReadWrite and WriteRead states;
+		File = FileAccess.Open(global.savePrefix + SaveFolder + SaveFile, FileAccess.ModeFlags.ReadWrite);
+	}
+
+	public override void _Process(double delta)
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	//save/load Functions
+	public void Load()
 	{
+		// load data from file
+	}
+	public void Save()
+	{
+		//save data to file
 	}
 }
--- Rbp-godot-game-src/Scripts/SceneScripts/Global.cs
+++ Rbp-godot-game-src/Scripts/SceneScripts/Global.cs
@@ -7,6 +7,7 @@ public partial class Global : Node
 	//Save Options
 	public string savePrefix;
 
+
 	//player ship data
 	public float ShipDir;//in dagrees
 	public Vector2 ShipPos;
