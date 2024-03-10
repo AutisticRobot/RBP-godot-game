1bc1726a742e2bf93b3c110fd0fdef597cffd9c7
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-03-09 20:40:45 -0800

    worked on creating SceneMan as base class

--- Rbp-godot-game-src/Scripts/SceneScripts/SceneMan.cs
+++ Rbp-godot-game-src/Scripts/SceneScripts/SceneMan.cs
@@ -1,20 +1,18 @@
 using Godot;
 using System;
 
-public partial class sceneMan : Node2D
+[GlobalClass]
+public partial class SceneMan : Node2D
 {
 	[Export] public SceneSave save;
 
 	public Global global;
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void ScenePrep()
 	{
 		global = GetNode<Global>("/root/Global");
 		save.global = global;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	extern public void _CloseScenePrep();
 }
