ae949b0adc2773658baedb412bff428778c1e646
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-03-13 20:59:54 -0700

    creaaated groundButton script

---
+++ Rbp-godot-game-src/Resorces/SceneObects/GroundButton.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class GroundButton : Area2D
+{
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
