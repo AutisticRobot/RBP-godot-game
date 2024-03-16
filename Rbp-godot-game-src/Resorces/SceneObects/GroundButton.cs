f26b2f42004108b51ab6ace635ef6785e9b2f8f3
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-03-15 21:50:47 -0700

    created PlayerOnButton bool and relaated signals

--- Rbp-godot-game-src/Resorces/SceneObects/GroundButton.cs
+++ Rbp-godot-game-src/Resorces/SceneObects/GroundButton.cs
@@ -4,14 +4,25 @@ using System;
 [GlobalClass]
 public partial class GroundButton : Area2D
 {
+	public bool PlayerOnButton = false;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
-	public void onAreaEnter()
+	public void onAreaEnter(Area2D enteredBody)
 	{
-		
+		if(enteredBody.Name == "PlayerArea")
+		{
+			PlayerOnButton = true;
+		}
+	}
+	public void onAreaExit(Area2D enteredBody)
+	{
+		if(enteredBody.Name == "PlayerArea")
+		{
+			PlayerOnButton = false;
+		}
 	}
 }
