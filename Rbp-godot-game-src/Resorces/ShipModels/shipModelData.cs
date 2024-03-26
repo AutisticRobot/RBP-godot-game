232927b3794d9c209f00e28227f705df6e4e48c0
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-03-25 21:28:35 -0700

    created ship model SSDebug

--- Rbp-godot-game-src/Resorces/ShipModels/shipModelData.cs
+++ Rbp-godot-game-src/Resorces/ShipModels/shipModelData.cs
@@ -1,10 +1,18 @@
 using Godot;
 using System;
 
+[GlobalClass]
 public partial class shipModelData : Resource
 {
     [Export] public Texture2D icoSprite;
+	[Export] public float brakeSpeed;
+	[Export] public float Acc;
+	[Export] public float TurnAcc;
+	[Export] public float Minspeed;
+	[Export] public float Maxspeed;
     [Export] public int maxHealth;
-    [Export] public int maxSpeed;
-    [Export] public inventory maxLoad;
+    [Export] public int maxWeight;
+    [Export] public int maxVolume;
+    [Export] public inventory LoadWeight;
+    [Export] public inventory LoadVolume;
 }
