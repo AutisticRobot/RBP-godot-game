fd527b7755181fe4a7e6326b06ca859cbaa3dabe
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-03-27 17:34:18 -0700

    updated island prefab to use ground button

--- Rbp-godot-game-src/Scripts/ObjectScripts/island.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/island.cs
@@ -1,53 +1,17 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public partial class island : Sprite2D
 {
-	[Export]
-	public int Money;
-	[Export]
-	public int Rum;
-	[Export]
-	public int Linens;
-	[Export]
-	public int Spices;
-	[Export]
-	public int Jewlery;
-	[Export]
-	public int FoodPrice;
-	[Export]
-	public int RumPrice;
-	[Export]
-	public int LinensPrice;
-	[Export]
-	public int SpicesPrice;
-	[Export]
-	public int JewleryPrice;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	[Export] public string islandID;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
-	public void AreaEntered(Area2D area)
-	{
-		PlayerShip ship = area.GetParent<PlayerShip>();
-		if(ship.player)
-		{
-			GetChild<Sprite2D>(1).Visible = true;
-		}
-	}
-	public void AreaExited(Area2D area)
-	{
-		PlayerShip ship = area.GetParent<PlayerShip>();
-		if(ship.player)
-		{
-			GetChild<Sprite2D>(1).Visible = false;
-		}
+	public Dictionary Data;
+	[Signal] public delegate Signal dockEventHandler(string IslandID);
 
+	public void onDock()
+	{
+		EmitSignal(SignalName.dock, islandID);
 	}
 }
