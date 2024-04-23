221e8912a3aa21ff6d85e50a82f45f2f09407f4a
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-04-22 20:48:31 -0700

    worked on portCaptain script and related materals

--- Rbp-godot-game-src/Scripts/HelperScripts/portCaptain.cs
+++ Rbp-godot-game-src/Scripts/HelperScripts/portCaptain.cs
@@ -1,6 +1,31 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public partial class portCaptain : Node
+public partial class portCaptain : Control
 {
+    public List<DockSpot> allDocks;
+	public override void _Ready()
+	{
+        Visible = false;
+
+        foreach(Node child in GetChildren())
+        {
+            if(child.GetType().ToString() == "DockSpot")
+            {
+                allDocks.Add((DockSpot)child);
+            }
+        }
+	}
+
+    public DockSpot getRandDock()
+    {
+        int length = allDocks.Count;
+
+        if(length > 0)
+        {
+            return allDocks[length];
+        }
+        return null;
+    }
 }
