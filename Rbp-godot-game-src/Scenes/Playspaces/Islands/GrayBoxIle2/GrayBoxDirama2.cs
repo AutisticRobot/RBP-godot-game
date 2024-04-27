8d48d92390e4a8e4e150f836fd1a495673413021
AutisticRobot <tmkormann04@gmail.com>
2024-04-27 09:39:53 -0700

    added grayboxV2 asset

---
+++ Rbp-godot-game-src/Scenes/Playspaces/Islands/GrayBoxIle2/GrayBoxDirama2.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class GrayBoxDirama : IslandClass
+{
+	[Signal] public delegate Signal shopOpenEventHandler(ShopInventory shopInv);
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		hideStandee();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void onShopOpen(ShopInventory shopInv)
+	{
+
+		EmitSignal(SignalName.shopOpen, shopInv);
+	}
+}
