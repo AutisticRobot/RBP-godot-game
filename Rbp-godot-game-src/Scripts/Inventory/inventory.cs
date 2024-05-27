1acabd82c332c36c73c30b944326066f0005d7f3
AutisticRobot <tmkormann04@gmail.com>
2024-05-26 20:53:26 -0700

    started conversion of inventory to new item system

--- Rbp-godot-game-src/Scripts/Inventory/inventory.cs
+++ Rbp-godot-game-src/Scripts/Inventory/inventory.cs
@@ -2,65 +2,22 @@ using Godot;
 using Godot.Collections;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 [GlobalClass]
 public partial class inventory : Resource, IEnumerable
 {
-	#region RESOURCES
-	[Export] public int Money;
-	[Export] public int Food;
-	[Export] public int Rum;
-	[Export] public int Linens;
-	[Export] public int Spices;
-	[Export] public int Jewlery;
-	#endregion
+	[Export] public List<Item> Items;
 
-	public int this[int i]
+	public Item this[int i]
 	{
 		get
 		{
-			return i switch
-			{
-				0 => Money,
-				1 =>Food,
-				2 =>Rum,
-				3 =>Linens,
-				4 =>Spices,
-				5 =>Jewlery,
-				_ => 0
-			};
+			return Items[i];
 		}
 		set
 		{
-			switch(i)
-			{
-                case 0:
-					Money = value;
-				break;
-
-                case 1:
-					Food = value;
-				break;
-
-                case 2:
-					Rum = value;
-				break;
-
-                case 3:
-					Linens = value;
-				break;
-
-                case 4:
-					Spices = value;
-				break;
-
-                case 5:
-					Jewlery = value;
-				break;
-				
-				default:
-				break;
-			}
+			Items[i] = value;
 		}
 	}
 	public int this[string i]
