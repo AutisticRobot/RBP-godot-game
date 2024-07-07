026dc764af0dddf84f1e414e98d45273f8adf123
AutisticRobot <tmkormann04@gmail.com>
2024-07-07 19:44:51 -0400

    crreated casting to and from string for shop item

--- Rbp-godot-game-src/Scripts/Inventory/ShopItem.cs
+++ Rbp-godot-game-src/Scripts/Inventory/ShopItem.cs
@@ -9,9 +9,26 @@ public partial class ShopItem : Item
     public ShopItem()
     {
     }
-    public ShopItem(int ID, int count)
+    public ShopItem(int ID, int count, int buy, int sell)
     {
         this.ID = ID;
         this.count = count;
+        buyPrice = buy;
+        SellPrice = sell;
+    }
+
+    static public implicit operator string(ShopItem item)
+    {
+        return "(" + item.ID + "," + item.count + "," + item.buyPrice + "," + item.SellPrice + ")";
+    }
+    static public explicit operator ShopItem(string inString)
+    {
+        //GD.Print("string to irem: " + inString);
+        string outstr = inString.Substr(1,inString.Length-2);
+        string[] dataArr = outstr.Split(",");
+
+
+        return new(dataArr[0].ToInt(),dataArr[1].ToInt(),dataArr[2].ToInt(),dataArr[3].ToInt());
+        
     }
 }
