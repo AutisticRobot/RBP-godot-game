966b4a4bb01c99933bf809ccf58e59df703755c6
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-08-02 23:28:26 -0700

    started planning implamentation for dock collisions

--- Rbp-godot-game-src/Scripts/ObjectScripts/DockSpot.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/DockSpot.cs
@@ -4,6 +4,7 @@ using System;
 [GlobalClass]
 public partial class DockSpot : Node2D
 {
+	[Export] public bool isfilled;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
