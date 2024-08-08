2d4e319c532e98c5172b2ad8629928ddfc0829c0
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-08-07 22:44:18 -0700

    fixed shop menu not showing listings (shopobj inv not being updated)

--- Rbp-godot-game-src/Scripts/ObjectScripts/shopObject.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/shopObject.cs
@@ -24,6 +24,7 @@ public partial class shopObject : Sprite2D
 	public void onShopOpen()
 	{
 		hardInv.flushInItems();
+		inv = hardInv;
 		//inv += modPrice.simpleMod((ShopInventory)hardInv.Duplicate(true));
 		inv.flushInItems();
 		if(hardInv[1] != null){GD.Print("hardInv:" + hardInv[1].SellPrice);}
