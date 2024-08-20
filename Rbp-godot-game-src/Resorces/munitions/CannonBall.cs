2f28e437b831eebe1a06815b7d4ed338471df615
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-08-19 18:38:12 -0700

    added gravity code to Cannonball

--- Rbp-godot-game-src/Resorces/munitions/CannonBall.cs
+++ Rbp-godot-game-src/Resorces/munitions/CannonBall.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class CannonBall : Node2D
 {
+	[Export] public float Gravity;
+	[Export] public bool ispaused;
+
 	public float Dir;//in dagrees
 	public float Speed;
 	public float Height;
+	public float VirVel;//virtical velocity
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -14,5 +18,20 @@ public partial class CannonBall : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(!ispaused)
+		{
+			VirVel -= Gravity;
+			Height -= VirVel;
+
+			splashCheck();
+		}
+	}
+
+	public void splashCheck()
+	{
+		if(Height <= 0)
+		{
+			Free();
+		}
 	}
 }
