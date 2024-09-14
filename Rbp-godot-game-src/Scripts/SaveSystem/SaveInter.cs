2c075c0a9cc8f688ea1543f6d6b6f27f7d57701d
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-09-13 21:40:28 -0700

    started work on save man code

--- Rbp-godot-game-src/Scripts/SaveSystem/SaveInter.cs
+++ Rbp-godot-game-src/Scripts/SaveSystem/SaveInter.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial interface SaveInter
 {
+    public void LoadIntoSaveMan(SaveMan man);
 
     public String ToString();
     public SaveInter FromString(String str);
--- Rbp-godot-game-src/Scripts/SaveSystem/SaveMan.cs
+++ Rbp-godot-game-src/Scripts/SaveSystem/SaveMan.cs
@@ -1,5 +1,19 @@
 
+using System;
+
 public partial class SaveMan
 {
-    
+    public Array saveObjs;
+
+    public bool addToBeSaved(SaveInter save)
+    {
+        try
+        {
+            saveObjs.SetValue(save, saveObjs.Length);
+            return true;
+        }catch{
+
+        }
+        return false;
+    }
 }
