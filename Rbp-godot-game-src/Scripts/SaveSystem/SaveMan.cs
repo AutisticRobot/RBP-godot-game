a00d1d551c2705c5cf6d61ebf614b0da466a7d34
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-09-14 18:10:26 -0700

    added encode and decode functions to SaveMan

--- Rbp-godot-game-src/Scripts/SaveSystem/SaveMan.cs
+++ Rbp-godot-game-src/Scripts/SaveSystem/SaveMan.cs
@@ -4,6 +4,7 @@ using System;
 public partial class SaveMan
 {
     public Array saveObjs;
+    public Array decodedData;
 
     public bool addToBeSaved(SaveInter save)
     {
@@ -15,5 +16,34 @@ public partial class SaveMan
 
         }
         return false;
+    }
+
+    public string Encode(bool doSafetyCheck = false)
+    {
+        string outData = "{";
+        bool firstObj = true;
+        foreach(SaveInter obj in saveObjs)
+        {
+            String strObj = obj.ToString();
+            if(doSafetyCheck)
+            {
+
+            }
+
+            if(!firstObj)
+            {
+                outData += ",";
+            }else{
+                firstObj = !firstObj;
+            }
+
+            outData += strObj;
+
+        }
+        return outData + "}";
+    }
+    public void Decode(string inData)
+    {
+
     }
 }
