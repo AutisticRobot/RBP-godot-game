b3ac1bba245257c933f45b7300e6458c542367c6
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-10-23 20:27:59 -0700

    created first function abstracts in diramaObj

---
+++ Rbp-godot-game-src/Scripts/SceneScripts/diramaObj.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+
+public abstract partial class diramaObj : Node2D
+{
+
+
+    public abstract string Encode();
+    public abstract string Decode();
+}
--- Rbp-godot-game-src/Scripts/SceneScripts/diramaObjInter.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-
-
-public partial class diramaObj : Node2D
-{
-
-}
