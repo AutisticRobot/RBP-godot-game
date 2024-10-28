2782d05ae610a645479284e5b22b43bc58d99282
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-10-27 23:38:35 -0700

    started work on decode function in diramaObj

--- Rbp-godot-game-src/Scripts/SceneScripts/diramaObjIns.cs
+++ Rbp-godot-game-src/Scripts/SceneScripts/diramaObjIns.cs
@@ -2,6 +2,8 @@ using Godot;
 using Godot.Collections;
 
 
+
+
 public class diramaObjIns
 {
     public string objTypeID;
@@ -9,18 +11,51 @@ public class diramaObjIns
     public Vector2 Pos;
     public Dictionary data;
     
+    diramaObjIns()
+    {
+        data = new();
+    }
+    diramaObjIns(Dictionary data)
+    {
+        this.data = data;
+    }
 
 
     public override string ToString()
     {
-        return null;
+        return Encode(this);
     }
-    public static string Encode(diramaObjIns ins)
+    public static string Encode(diramaObjIns ins, bool sanitizeInput = true)
     {
-        return ins.ToString();
+        string outstr = "";
+        return outstr;
     }
     public static diramaObjIns Decode(string inData)
     {
-        return new();
+		if(inData.Length <= 2)
+		{
+			return null;
+		}
+		Dictionary outdat = new();
+		string inStr = inData;
+        int layer = 0;
+
+        foreach(char a in inStr)
+        {
+            switch(a)
+            {
+            case'{':
+            layer ++;
+            break;
+            case'}':
+            layer--;
+            break;
+            default:
+            break;
+            };
+
+        }
+
+        return new(outdat);
     }
 }
