fb56aec728de97d2496bd88438b551738032b756
Tyler M Kormann <tmkormann04@gmail.com>
2025-01-05 22:31:28 -0800

    worked on the start of the status script

--- Rbp-godot-game-src/Scripts/Inventory/status.cs
+++ Rbp-godot-game-src/Scripts/Inventory/status.cs
@@ -1,7 +1,16 @@
 
+using System.Collections.Generic;
 using Godot;
 
 public partial class status : Resource
 {
+    [Export] public List<string> effects;
+
     [Export] public int health;
+    [Export] public float AccModPer;
+
+    public void evalEff()
+    {
+        
+    }
 }
