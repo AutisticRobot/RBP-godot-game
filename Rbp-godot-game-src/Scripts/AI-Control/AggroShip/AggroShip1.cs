75112687b7442097dac4db7a6f3ba81c23a4ee29
Tyler M Kormann <tmkormann04@gmail.com>
2025-01-10 17:12:11 -0800

    Created aggroship1 script

---
+++ Rbp-godot-game-src/Scripts/AI-Control/AggroShip/AggroShip1.cs
@@ -0,0 +1,34 @@
+using Godot;
+
+public partial class AggroShip1 : shipInput
+{
+    public bool getAncorState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector2 getCannonTarget()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float getSailCom()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float getTurnDir()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool isFireCannon()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void setShip(Ship ship)
+    {
+        throw new System.NotImplementedException();
+    }
+}
