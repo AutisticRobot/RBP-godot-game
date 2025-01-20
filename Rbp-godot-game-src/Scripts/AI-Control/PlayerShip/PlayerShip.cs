291865b027437c4013b73a81f811f6462700e273
Tyler M Kormann <tmkormann04@gmail.com>
2025-01-19 19:46:04 -0800

    added code to player ship respawn func

--- Rbp-godot-game-src/Scripts/AI-Control/PlayerShip/PlayerShip.cs
+++ Rbp-godot-game-src/Scripts/AI-Control/PlayerShip/PlayerShip.cs
@@ -18,7 +18,7 @@ public partial class PlayerShip : Node2D// : Ship
 			 public Global global; 
 
 	[Export] public string ShipModelID;
-
+	[Export] public RespawnPoint curRespawnPoint;
 
 
     public override void _PhysicsProcess(double delta)
@@ -98,155 +98,8 @@ public partial class PlayerShip : Node2D// : Ship
 
 	public void Respawn()
 	{
-
+		Position = curRespawnPoint.Position;
+		playerDed = false;
 	}
 
-	/*===========OLD SCRIPT FOR REFERENCE==============
-	[Export] public float brakeSpeed;
-	[Export] public float Acc;
-	[Export] public float TurnAcc;
-			 public float speed;
-	[Export] public float Minspeed;
-	[Export] public float Maxspeed;
-	[Export] public float dir;//in Dagrees
-
-	[Export] public MunitionRes ammoData;
-	[Export] public float ammoSpeed;
-	[Export] public string cannonBallUUID;
-	[Export] public float gunCooldown;
-	[Export] public float gunState;
-	[Export] public Vector2 cannonOffset;
-
-	[Export] public inventory inv;
-	[Export] public SceneSave save;
-		public Dictionary data;
-
-
-	private Global global; 
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		manager = GetOwner<SceneMan>();
-
-		LoadShipModel();
-
-		//inv ??= new();
-
-		if(player)
-		{
-			global = GetNode<Global>("/root/Global");
-		
-			if(global.playerDataFilled == true)
-			{
-				dir = global.ShipDir;
-				Position = global.ShipPos;
-				inv = global.playerHull;
-				GD.Print("get Inv Global");
-		
-
-			}
-		}
-
-
-	}
-
-	// Called every frame. 'delta' is the elapsed time sinc
-e the previous frame.
-	public override void _Process(double delta)
-	{
-		//inv ??= new();
-		if(!manager.pausedScene)
-		{
-			Pinput();
-        	Vector2 vel = new()
-        	{
-        	    X = (float)(Math.Sin(dir * (Math.PI / 180)) * speed * delta),
-        	    Y = (float)(Math.Cos(dir * (Math.PI / 180)) * speed * delta)
-        	};
-			//GD.Print(speed);
-			Velocity = vel;
-			MoveAndSlide();
-			gunState -= (float)delta;
-		}
-		if(player)
-		{
-			Save();//this should use signals for the sake of expanability and performance; because saving every frame is bad
-		}
-		global.playerHull = inv;//this should use signals for the sake of expanability and performance; same reason here
-			
-	}
-
-	public void Save()
-	{
-		global.ShipDir = dir;
-		global.ShipPos = Position;
-		global.playerDataFilled = true;
-
-	}
-
-	private void Pinput()
-	{/*
-		if(Input.IsActionPressed("Accelerate"))
-		{
-			speed += Acc;
-		}
-		if(Input.IsActionPressed("Reverse"))
-		{
-			speed -= Acc;
-		}
-		if(Input.IsActionPressed("Tccw"))
-		{
-			dir += TurnAcc;
-		}
-		if(Input.IsActionPressed("Tcw"))
-		{
-			dir -= TurnAcc;
-		}
-		if(Input.IsActionPressed("Brake"))
-		{
-			if(-brakeSpeed <= speed && speed <= brakeSpeed){speed = 0;}//set speed to 0 when braking at low speeds
-			if(speed > 0){speed -= brakeSpeed;}//brake when reversing
-			if(speed < 0){speed += brakeSpeed;}//brake when going forwoard
-			
-		}
-		if(Input.IsActionPressed("L-click"))
-		{
-			MoveTowardCursor();
-		}
-		speed = Math.Clamp(speed, Minspeed, Maxspeed);
-		if(Input.IsActionJustPressed("Shoot"))
-		{
-			if(gunState <= 0)//cooldown check
-			{
-				//FireCannons();
-			}
-		}
-	}
-
-
-	public void MoveTowardCursor()
-	{
-		Vector2 relTar = (cursor.Position - Position);//.Normalized();
-
-		dir = (float)(Math.Atan2(relTar.X, relTar.Y) * (180/Math.PI));
-
-	}
-
-	public void LoadShipModel()
-	{
-		Node model = ShipModel.Instantiate();
-		AddChild(model);
-		foreach(Node node in model.GetChildren())
-		{
-			node.Owner = this;
-			node.Reparent(this);
-		}
-		model.QueueFree();
-
-	}
-
-
-
-*/
 }
--- Rbp-godot-game-src/Scripts/ObjectScripts/RespawnPoint.cs
+++ Rbp-godot-game-src/Scripts/ObjectScripts/RespawnPoint.cs
@@ -1,7 +1,7 @@
 using Godot;
 
 [GlobalClass]
-partial class RespawnPoint : Node2D
+public partial class RespawnPoint : Node2D
 {
     public override void _Ready()
     {
