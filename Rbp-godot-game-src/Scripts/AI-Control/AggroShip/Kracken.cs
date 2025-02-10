6855315abf637800b966811df2ef8f53ab2ed2fd
Tyler M Kormann <tmkormann04@gmail.com>
2025-02-09 19:39:11 -0800

    added tents varrible to Kracken script

--- Rbp-godot-game-src/Scripts/AI-Control/AggroShip/Kracken.cs
+++ Rbp-godot-game-src/Scripts/AI-Control/AggroShip/Kracken.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Kracken : SeaMonsterBase
 {
+	[Export] public Godot.Collections.Array<SeaMonsterBase> tents;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
